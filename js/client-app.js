dc06d1e3726ec9cebbfcb87e8cfc874676211234
caylazabel <caylazabel@gmail.com>
2017-05-03 13:34:26 -0700

    new component

---
+++ js/client-app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyFirstComponent = React.createClass({
+  render: function (){
+    return (
+      div(null,
+      h1(null, 'This is my first component!')
+    )
+  )
+}
+})
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
