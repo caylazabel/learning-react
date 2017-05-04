1149d2eca86945912c5db924d556459ac14bff4d
caylazabel <caylazabel@gmail.com>
2017-05-04 10:38:16 -0700

    webpack added

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,19 +1,10 @@
-/* global React ReactDOM */
+import React from 'react';
+import { render } from 'react-dom';
+import MyTitle from './MyTitle';
 
-var div = React.DOM.div
-var h1 = React.DOM.h1
+var div = React.DOM.div;
 
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-        h1({style: {color: this.props.color}}, this.props.title)
-      )
-    )
-  }
-})
-
-var MyTitleFactory = React.createFactory(MyTitle)
+var MyTitleFactory = React.createFactory(MyTitle);
 
 var MyFirstComponent = React.createClass({
   render: function () {
@@ -24,8 +15,8 @@ var MyFirstComponent = React.createClass({
         MyTitleFactory({ title: 'Hey whats  up', color: 'yellow' }),
         MyTitleFactory({ title: 'its sunny outside', color: 'green' })
       )
-    )
+    );
   }
-})
+});
 
-ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
+render(React.createElement(MyFirstComponent), document.getElementById('app'));
