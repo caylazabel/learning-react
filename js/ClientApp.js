b2c9e8d3cfcf07e2958053209ee9ed0473290701
caylazabel <caylazabel@gmail.com>
2017-05-04 14:48:16 -0700

    css

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,19 +1,3 @@
 import React from 'react';
 import { render } from 'react-dom';
-import MyTitle from './MyTitle';
-
-
-
-var MyFirstComponent = React.createClass({
-  render: function () {
-    return (
-      <div>
-      <MyTitle title='props are cool' color='lightPink' />
-      <MyTitle title='this is dope' color='lightBlue' />
-      <MyTitle title='its so sunny outside' color='lightYellow' />
-      </div>
-    );
-  }
-});
-
-render(React.createElement(MyFirstComponent), document.getElementById('app'));
+import 
