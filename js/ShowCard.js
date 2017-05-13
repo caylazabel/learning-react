fa46e611102b8224d7d903656685346a41cc58e8
caylazabel <caylazabel@gmail.com>
2017-05-13 13:42:53 -0700

    added header

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,27 +1,25 @@
 import React from 'react'
-const { shape, string } = React.PropTypes
+const { string } = React.PropTypes
 
 const ShowCard = React.createClass({
   propTypes: {
-    show: shape({
-      poster: string,
-      title: string,
-      year: string,
-      description: string.isRequired
-    }),
-    render () {
-      const { poster, title, year, description } = this.props.show
-      return (
-        <div className='show-card'>
-          <img src={`/public/img/posters/${poster}`} />
-          <div>
-            <h3>{title}</h3>
-            <h4>({year})</h4>
-            <p>{description}</p>
-          </div>
+    poster: string,
+    title: string,
+    year: string,
+    description: string
+  },
+  render () {
+    const { poster, title, year, description } = this.props
+    return (
+      <div className='show-card'>
+        <img src={`/public/img/posters/${poster}`} />
+        <div>
+          <h3>{title}</h3>
+          <h4>({year})</h4>
+          <p>{description}</p>
         </div>
-      )
-    }
+      </div>
+    )
   }
 })
 
