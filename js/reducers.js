5cba6e42064633122cf1fcd1c2c4e2150cb392f9
caylazabel <caylazabel@gmail.com>
2017-05-23 11:56:13 -0700

    reducers file added search term

--- js/reducers.js
+++ js/reducers.js
@@ -1,10 +1,13 @@
+import { SET_SEARCH_TERM } from './actions'
+
 const DEFAULT_STATE = {
   searchTerm: ''
 }
 
 const rootReducer = (state = DEFAULT_STATE, action) => {
-  state = state || DEFAULT_STATE
   switch (action.type) {
+    case SET_SEARCH_TERM:
+      return setSearchTerm(state, action)
     default:
       return state
   }
