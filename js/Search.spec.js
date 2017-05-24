eb0ed70dc164593ff7a352785132c124ebc95cf8
caylazabel <caylazabel@gmail.com>
2017-05-23 22:34:25 -0700

    search spec tests fixed

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -15,6 +15,7 @@ test('Search should render a ShowCard for each show', () => {
   const component = shallow(<Search />)
   expect(component.find(ShowCard).length).toEqual(preload.shows.length)
 })
+;
 
 test('Search should render correct amount of shows based on search', () => {
   const searchWord = 'house'
