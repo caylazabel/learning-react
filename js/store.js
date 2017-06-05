f734f97ea33e3e9c15626bb5b6a5d0c9170b4c9f
caylazabel <caylazabel@gmail.com>
2017-06-05 15:18:18 -0700

    store js file changed

--- js/store.js
+++ js/store.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, compose(
+  typeOf window === 'object' && typeOf window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : (f) => f
+))
 
 export default store
