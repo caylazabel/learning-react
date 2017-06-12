e9db268af45e449be3ec960f2f23169d8bae8999
caylazabel <caylazabel@gmail.com>
2017-06-12 14:49:01 -0700

    added addOMDB data redux

--- js/reducers.js
+++ js/reducers.js
@@ -14,12 +14,18 @@ const setSearchTerm = (state, action) => {
 const addOMDBData= (state, action) => {
   const newOMDBData = {}
   Object.assign(newOMDBData, state.omdbData, {[action.imdbID]: action.omdbData})
+  const newState = {
+  Object.assign(newState, state, {omdbData: newOMDBData})
+  return newState
+  }
 }
 
 const rootReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case SET_SEARCH_TERM:
       return setSearchTerm(state, action)
+      case ADD_OMDB_DATA:
+      return addOMDBData(state, action)
     default:
       return state
   }
