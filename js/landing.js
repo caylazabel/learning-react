8b7e7e4e59c62809403e462049f3dc8e53f8f192
caylazabel <caylazabel@gmail.com>
2017-08-28 22:15:21 -0400

    sup

--- js/landing.js
+++ js/landing.js
@@ -26,7 +26,7 @@ const Landing = React.createClass({
         <form onSubmit={this.handleSearchSubmit}>
           <input onChange={this.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='Search' />
         </form>
-        <Link to='/search'>or Browse All.</Link>
+        <Link to='/search'>or Browse All...</Link>
       </div>
      )
   }
